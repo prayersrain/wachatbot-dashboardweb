21073a9723f615827248fdfacf1b752e833c18ec
suhail <suhailczy@gmail.com>
2026-05-15 15:35:15 +0700

    icon pwa

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['favicon.png', 'apple-touch-icon.png', 'logoyoyobolen.PNG'],
       manifest: {
         name: 'Yoyo Bakery Backoffice',
         short_name: 'YoyoAdmin',
